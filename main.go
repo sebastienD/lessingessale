bbbd7a7140f8e47ca2727d1d379955dc72b4f82b
sebastienD <sebastien.descamps@gmail.com>
2017-02-23 20:38:28 +0100

    wip

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func check(e error) {
 }
 
 func main() {
-	f, err := os.Open("me_at_the_zoo.in")
+	f, err := os.Open("me_at_the_zoo_example.in")
 	check(err)
 	defer f.Close()
 
@@ -26,14 +26,72 @@ func main() {
 	scanner.Scan()
 	line := strings.Split(scanner.Text(), " ")
 
-	// convert params to []int
+	// first line
+	//nbVideos := convert2int(line[0])
+	nbEndpoints := convert2int(line[1])
+	nbRequestDesc := convert2int(line[2])
+	nbCaches := convert2int(line[3])
+	cachSize := convert2int(line[4])
+
 	entry := Entry{
-		Videos:      convert2int(line[0]),
-		Endpoints:   convert2int(line[1]),
-		RequestDesc: convert2int(line[2]),
-		NbCache:     convert2int(line[3]),
-		CacheSize:   convert2int(line[4]),
+		Videos:      []Video{},
+		Endpoints:   []Endpoint{},
+		RequestDesc: nbRequestDesc,
+		Requests:    []Request{},
+		Caches:      []Cache{},
 	}
+	fmt.Printf("%+v\n", entry)
+
+	for i := 0; i < nbCaches; i++ {
+		entry.Caches = append(entry.Caches, Cache{
+			Capacity: cachSize,
+		})
+	}
+
+	// second line
+	scanner.Scan()
+	for _, o := range strings.Split(scanner.Text(), " ") {
+		entry.Videos = append(entry.Videos, Video{
+			Size: convert2int(o),
+		})
+	}
+
+	// second line
+	//  Parse endpoints
+	for n := 0; n < nbEndpoints; n++ {
+		scanner.Scan()
+		split := strings.Split(scanner.Text(), " ")
+		nbLatencies := convert2int(split[1])
+
+		endpoint := Endpoint{latencyToDatacenter: convert2int(split[0])}
+		for i := 0; i < nbLatencies; i++ {
+			scanner.Scan()
+			split = strings.Split(scanner.Text(), " ")
+			latence := Latency{}
+			latence[convert2int(split[0])] = convert2int(split[1])
+			endpoint.Latencies = append(endpoint.Latencies, latence)
+		}
+
+		entry.Endpoints = append(entry.Endpoints, endpoint)
+	}
+
+	fmt.Println(entry.Endpoints)
+
+	for k := 0; k < nbRequestDesc; k++ {
+		scanner.Scan()
+		split := strings.Split(scanner.Text(), " ")
+
+		endpoint := entry.Endpoints[convert2int(split[1])]
+		video := entry.Videos[convert2int(split[0])]
+		nbRequests := convert2int(split[2])
+
+		entry.Requests = append(entry.Requests, Request{
+			Endpoint: endpoint,
+			Nb:       nbRequests,
+			Video:    video,
+		})
+	}
+
 	fmt.Println(entry)
 }
 
@@ -74,4 +132,5 @@ type Entry struct {
 	Endpoints   []Endpoint
 	Caches      []Cache
 	RequestDesc int
+	Requests    []Request
 }
