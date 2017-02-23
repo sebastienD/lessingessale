fadaf82ce028a7d88c7e6bdfd36d3541a189951a
sdescamps <sebastien.descamps@gmail.com>
2017-02-23 19:13:34 +0100

    feat: push initial entry struct

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "log"
+
+func main() {
+	log.Println("plop")
+}
+
+type Entry struct {
+	Videos      int
+	Endpoints   int
+	RequestDesc int
+	NbCache     int
+	CacheSize   int
+}
